f81d584238d3fa98ab34b4c1ec613cdc4f93452a
AhmedSayeddd <ahmedsayedd1101@gmail.com>
2026-01-07 04:03:12 +0200

    sura details screen created

---
+++ lib/screen/sura_details/sura_details_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SuraDetailsScreen extends StatelessWidget {
+  const SuraDetailsScreen({super.key});
+  static const String routeName = "SuraDetailsScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
